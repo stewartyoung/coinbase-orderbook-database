09ee0fbc069fb6f0844dec3a73cec790b4ae169a
Stewart Young <stewartyoung97@gmail.com>
2022-01-30 13:08:31 +0000

    successful mock test on onError()

--- src/test/java/io/stewartyoung/gsr/websockets/WebsocketClientEndpointTest.java
+++ src/test/java/io/stewartyoung/gsr/websockets/WebsocketClientEndpointTest.java
@@ -1,5 +1,9 @@
 package io.stewartyoung.gsr.websockets;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.read.ListAppender;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.glassfish.tyrus.client.ClientManager;
@@ -8,12 +12,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.slf4j.LoggerFactory;
 
 import javax.websocket.DeploymentException;
 import javax.websocket.Session;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -62,6 +68,19 @@ public class WebsocketClientEndpointTest {
     public void testOnClose() {
         websocketClientEndpoint.close();
         assertEquals(websocketClientEndpoint.getUserSession(), null);
+    }
+
+    @Test
+    public void testOnError() {
+        Logger websocketClientEndpointLogger = (Logger) LoggerFactory.getLogger(WebsocketClientEndpoint.class);
+        websocketClientEndpointLogger.setLevel(Level.ERROR);
+        ListAppender<ILoggingEvent> listAppender = new ListAppender<>();
+        listAppender.start();
+        websocketClientEndpointLogger.addAppender(listAppender);
+        List<ILoggingEvent> logsList = listAppender.list;
+
+        websocketClientEndpoint.onError(session, new Throwable());
+        assertTrue(logsList.get(0).getMessage().contains("Error for the session"));
     }
 
 }
